72bcb4bf27810f4fab56c45cb476573585a77d1a
Xiangdong Gu <xgu@massmutual.com>
2018-06-12 11:35:16 +0800

    more versions of java

--- R/hello.R
+++ R/hello.R
@@ -4,10 +4,14 @@
 #' @param pass password
 #' @export
 #'
-mssql_con <- function(host, user, pass) {
+mssql_con <- function(host, user, pass, java_version = c("9", "8", "7")) {
   require(RJDBC)
+  if (lenght(java_version) > 1) java_version <- java_version[1]
   drv_path <- system.file("driver", package = "mssqlr")
-  drv_path <- file.path(drv_path, "mssql-jdbc-6.4.0.jre9.jar")
+  drv_path <- file.path(
+    drv_path,
+    springf("mssql-jdbc-6.4.0.jre%s.jar", java_version)
+  )
   drv <- JDBC("com.microsoft.sqlserver.jdbc.SQLServerDriver", drv_path)
   conn <- dbConnect(
     drv,
