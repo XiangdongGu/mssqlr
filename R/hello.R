774bf3f42cd0f31f7f1f9e865cd2458402f075a1
Xiangdong Gu <xgu@massmutual.com>
2018-06-12 11:42:44 +0800

    typo

--- R/hello.R
+++ R/hello.R
@@ -10,7 +10,7 @@ mssql_con <- function(host, user, pass, java_version = c("9", "8", "7")) {
   drv_path <- system.file("driver", package = "mssqlr")
   drv_path <- file.path(
     drv_path,
-    springf("mssql-jdbc-6.4.0.jre%s.jar", java_version)
+    sprintf("mssql-jdbc-6.4.0.jre%s.jar", java_version)
   )
   drv <- JDBC("com.microsoft.sqlserver.jdbc.SQLServerDriver", drv_path)
   conn <- dbConnect(
